15288fb6009a7b5b74277e1e6fcf665ec4f442fb
William <Wrilliam@users.noreply.github.com>
2017-11-02 19:34:03 -0300

    Algumas atualizações ao site

--- SMD0092/script.js
+++ SMD0092/script.js
@@ -1,7 +1,6 @@
-function open() {
-    document.getElementById("sidebar").style.width = "250px";
-}
-
-function close() {
-    document.getElementById("sidebar").style.width = "0";
+function toggleNav() {
+	if(document.getElementById("sidebar").style.width == "200px")
+		document.getElementById("sidebar").style.width = "0";
+	else
+		document.getElementById("sidebar").style.width = "200px";
 }
