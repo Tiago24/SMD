3089107da4b16d826c197b1225805d17c470ac02
William <Wrilliam@users.noreply.github.com>
2017-11-02 20:30:32 -0300

    Mais alguns ajustes
    E diminuindo a zuera

--- SMD0092/script.js
+++ SMD0092/script.js
@@ -1,6 +1,6 @@
 function toggleNav() {
-	if(document.getElementById("sidebar").style.width == "200px")
+	if(document.getElementById("sidebar").style.width == "150px")
 		document.getElementById("sidebar").style.width = "0";
 	else
-		document.getElementById("sidebar").style.width = "200px";
+		document.getElementById("sidebar").style.width = "150px";
 }
