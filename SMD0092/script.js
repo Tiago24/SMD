1264b161ac23d89bce24bcb52e7ac25266c6cc73
William <Wrilliam@users.noreply.github.com>
2017-11-24 17:37:42 -0300

    Vários reajustes
    Vários MESMO. Algumas páginas foram reformuladas.

--- SMD0092/script.js
+++ SMD0092/script.js
@@ -1,6 +1,8 @@
 function toggleNav() {
-	if(document.getElementById("sidebar").style.width == "150px")
+	if(document.getElementById("sidebar").style.width == "150px") {
 		document.getElementById("sidebar").style.width = "0";
-	else
+	}
+	else {
 		document.getElementById("sidebar").style.width = "150px";
+	}
 }
